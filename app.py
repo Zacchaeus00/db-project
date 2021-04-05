f5760682716ca93ee0144446a5987abad008eafc
Zacchaeus <64816317+Zacchaeus14@users.noreply.github.com>
2021-04-05 18:42:47 +0800

    added public page
    implemented Search for upcoming flights and Check flight status:

--- app.py
+++ app.py
@@ -6,27 +6,36 @@ db = mysql.connector.connect(host='localhost',user = 'root',password = '123456',
 
 
 @app.route("/")
-def index():
+def public():
 	scity = request.args.get('scity')
 	dcity = request.args.get('dcity')
-	dept_date = request.args.get('dept_date')
-	if not scity:
-		return render_template('index.html', flights=None)
+	date_search = request.args.get('date_search')
+	flight_num = request.args.get('flight_num')
+	dateof = request.args.get('dateof')
+	date_check = request.args.get('date_check')
+	flights = None
+	statuses = None
 	cursor = db.cursor()
-	query = """select * from flight, airport a1, airport a2 
-			where departure_airport = a1.airport_name and a1.airport_city = '{}'
-			and arrival_airport = a2.airport_name and a2.airport_city = '{}'
-			and departure_time LIKE '{}%' and status = 'upcoming'"""
-	cursor.execute(query.format(scity, dcity, dept_date))
-	flights = cursor.fetchall()
+	if scity:
+		query = """select * from flight, airport a1, airport a2 
+				where departure_airport = a1.airport_name and a1.airport_city = '{}'
+				and arrival_airport = a2.airport_name and a2.airport_city = '{}'
+				and departure_time LIKE '{}%' and status = 'upcoming'"""
+		cursor.execute(query.format(scity, dcity, date_search))
+		flights = cursor.fetchall()
+	if flight_num:
+		query = """select status from flight
+				where flight_num = '{}' and {}_time LIKE '{}%'"""
+		cursor.execute(query.format(flight_num, dateof, date_check))
+		statuses = cursor.fetchall()
 	cursor.close()
-	return render_template('index.html', flights=flights)
+	return render_template('public.html', flights=flights, statuses=statuses)
 	
 @app.route("/login")
 def login():
-	pass
+	return "to be implemented"
 	
 @app.route("/signup")
 def signup():
-	pass
+	return "to be implemented"
 	

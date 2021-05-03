51a774998eaa7ad557a0e01a2c5ea9e5538ece12
Zacchaeus <64816317+Zacchaeus14@users.noreply.github.com>
2021-05-03 21:43:46 +0800

    all functions for staff done

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import mysql.connector
 import json
 import datetime
 import calendar
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import time
+import os
+import glob
+import numpy as np
+mpl.use('Agg')
+mpl.is_interactive()
 app = Flask(__name__)
 dbconfig = json.load(open("dbconfig.json"))
 conn = mysql.connector.connect(host=dbconfig['host'], user=dbconfig['user'],
@@ -628,28 +636,300 @@ def staff_add_airport():
 
 @ app.route("/staff_view_booking_agents")
 def staff_view_booking_agents():
-    return "TODO"
+    if "usertype" not in session:
+        return redirect("/")
+    if session["usertype"] != "staff":
+        return redirect("/")
+    cursor = conn.cursor()
+    query = """select booking_agent.*, count(*)
+            from booking_agent natural join purchases natural join ticket
+            where purchase_date > '{}'
+            and airline_name = '{}'
+            group by email
+            order by count(*) DESC
+            limit 5
+            """
+    last_month = add_months(datetime.date.today(), -1).strftime('%Y-%m-%d')
+    cursor.execute(query.format(last_month, session["airline_name"]))
+    data1 = cursor.fetchall()
+    data1 = [[attr for attr in agent] for agent in data1]
+    # remove password
+    for i in range(len(data1)):
+        del data1[i][1]
+
+    last_year = add_months(datetime.date.today(), -12).strftime('%Y-%m-%d')
+    cursor.execute(query.format(last_year, session["airline_name"]))
+    data2 = cursor.fetchall()
+    data2 = [[attr for attr in agent] for agent in data2]
+    # remove password
+    for i in range(len(data2)):
+        del data2[i][1]
+
+    query = """select booking_agent.*, sum(price)*0.1
+            from booking_agent natural join purchases natural join ticket natural join flight
+            where purchase_date > '{}'
+            and airline_name = '{}'
+            group by email
+            order by sum(price) DESC
+            limit 5
+            """
+    cursor.execute(query.format(last_year, session["airline_name"]))
+    data3 = cursor.fetchall()
+    data3 = [[attr for attr in agent] for agent in data3]
+    # remove password
+    for i in range(len(data3)):
+        del data3[i][1]
+
+    cursor.close()
+    kwargs = dict(
+        data1=data1,
+        date1=last_month,
+        data2=data2,
+        date2=last_year,
+        data3=data3,
+        date3=last_year
+    )
+    return render_template("staff_view_booking_agents.html", **kwargs)
 
 
 @ app.route("/staff_view_frequent_customers")
 def staff_view_frequent_customers():
-    return "TODO"
+    if "usertype" not in session:
+        return redirect("/")
+    if session["usertype"] != "staff":
+        return redirect("/")
+    last_year = add_months(datetime.date.today(), -12).strftime('%Y-%m-%d')
+    cursor = conn.cursor()
+    query = """
+            select customer.*, count(*)
+            from customer, purchases, ticket
+            where customer.email = purchases.customer_email
+            and ticket.ticket_id = purchases.ticket_id
+            and airline_name = '{}'
+            and purchase_date > '{}'
+            group by email
+            order by count(*) DESC
+            limit 1
+            """
+    cursor.execute(query.format(session["airline_name"], last_year))
+    top_customer = cursor.fetchall()[0]
+    top_customer = [str(attr) for attr in top_customer]
+    del top_customer[2]
+
+    email = request.args.get("email")
+    query = """
+            select distinct flight.*
+            from flight natural join purchases natural join ticket
+            where customer_email = '{}'
+            and airline_name = '{}'
+            """
+    cursor.execute(query.format(email, session["airline_name"]))
+    flights = cursor.fetchall()
+    flights = [[str(attr) for attr in flight] for flight in flights]
+
+    cursor.close()
+    kwargs = dict(
+        top_customer=top_customer,
+        last_year=last_year,
+        my_airline=session["airline_name"],
+        flights=flights,
+        email=email
+    )
+    return render_template("staff_view_frequent_customers.html", **kwargs)
 
 
 @ app.route("/staff_view_reports")
 def staff_view_reports():
-    return "TODO"
+    if "usertype" not in session:
+        return redirect("/")
+    if session["usertype"] != "staff":
+        return redirect("/")
+    last_year = add_months(datetime.date.today(), -12).strftime('%Y-%m-%d')
+    cursor = conn.cursor()
+    query = """
+            select count(*)
+            from purchases natural join ticket
+            where airline_name = '{}'
+            and purchase_date > '{}'
+            """
+    cursor.execute(query.format(session["airline_name"], last_year))
+    amt_tck_ly = str(cursor.fetchall()[0][0])
+
+    last_month = add_months(datetime.date.today(), -1).strftime('%Y-%m-%d')
+    cursor.execute(query.format(session["airline_name"], last_month))
+    amt_tck_lm = str(cursor.fetchall()[0][0])
+
+    start_date = request.args.get("start_date")
+    end_date = request.args.get("end_date")
+    amt_tck_range = None
+    if start_date:
+        query = """
+                select count(*)
+                from purchases natural join ticket
+                where airline_name = '{}'
+                and (purchase_date between '{} 00:00' and '{} 23:59')
+                """
+        cursor.execute(query.format(
+            session["airline_name"], start_date, end_date))
+        amt_tck_range = str(cursor.fetchall()[0][0])
+
+    year = request.args.get("year")
+    img_url = ""
+    if year:
+        # clear history images
+        dir = './static/staff_view_reports/'
+        filelist = glob.glob(os.path.join(dir, "*"))
+        for f in filelist:
+            os.remove(f)
+        image_name = str(time.time()).split(
+            '.')[-1] + '.png'  # avoid image caching
+        img_url = os.path.join(dir, image_name)
+        sales = []
+        for month in range(1, 13):
+            query = """
+                    select count(*)
+                    from purchases natural join ticket
+                    where year(purchase_date) = '{}'
+                    and month(purchase_date) = '{}'
+                    and airline_name = '{}'
+                    """
+            cursor.execute(query.format(year, month, session["airline_name"]))
+            sales.append(int(cursor.fetchall()[0][0]))
+        x = [m for m in range(1, 13)]
+        fig, axes = plt.subplots(1, 1)
+        axes.bar(x, sales)
+        axes.xaxis.set_ticks(x)
+        axes.set_title(f"Monthly sales in {year}")
+        axes.set_xlabel("month")
+        axes.set_ylabel("sales")
+        plt.savefig(img_url)
+
+    cursor.close()
+
+    current_year = datetime.date.today().strftime('%Y-%m-%d')[:4]
+    kwargs = dict(
+        amt_tck_ly=amt_tck_ly,
+        amt_tck_lm=amt_tck_lm,
+        start_date=start_date,
+        end_date=end_date,
+        amt_tck_range=amt_tck_range,
+        current_year=current_year,
+        img_url=img_url,
+        year=year
+    )
+    # return json.dumps(kwargs)
+    return render_template("staff_view_reports.html", **kwargs)
 
 
 @ app.route("/staff_compare_revenue")
 def staff_compare_revenue():
-    return "TODO"
+    if "usertype" not in session:
+        return redirect("/")
+    if session["usertype"] != "staff":
+        return redirect("/")
+    cursor = conn.cursor()
+    query = """
+            select sum(price)
+            from purchases natural join ticket natural join flight
+            where airline_name = '{}'
+            and booking_agent_id {} NULL
+            and purchase_date > '{}'
+            """
+
+    last_year = add_months(datetime.date.today(), -12).strftime('%Y-%m-%d')
+    last_month = add_months(datetime.date.today(), -1).strftime('%Y-%m-%d')
+
+    cursor.execute(query.format(session["airline_name"], "is", last_year))
+    res = cursor.fetchall()[0][0]
+    direct_ly = 0 if not res else int(res)
+
+    cursor.execute(query.format(session["airline_name"], "is not", last_year))
+    res = cursor.fetchall()[0][0]
+    indirect_ly = 0 if not res else int(res)
+
+    cursor.execute(query.format(session["airline_name"], "is", last_month))
+    res = cursor.fetchall()[0][0]
+    direct_lm = 0 if not res else int(res)
+
+    cursor.execute(query.format(session["airline_name"], "is not", last_month))
+    res = cursor.fetchall()[0][0]
+    indirect_lm = 0 if not res else int(res)
+
+    cursor.close()
+
+    dir = './static/staff_compare_revenue/'
+    filelist = glob.glob(os.path.join(dir, "*"))
+    for f in filelist:
+        os.remove(f)
+    image_name = str(time.time()).split(
+        '.')[-1] + '.png'  # avoid image caching
+    img_url = os.path.join(dir, image_name)
+
+    x1 = [direct_ly, indirect_ly]
+    x2 = [direct_lm, indirect_lm]
+    labels = ["direct_sales", "indirect_sales"]
+
+    fig, axes = plt.subplots(2)
+
+    def func(pct, allvals):
+        absolute = int(pct/100.*np.sum(allvals))
+        return "{:.1f}%\n({:d} $)".format(pct, absolute)
+
+
+    wedges, texts, autotexts = axes[0].pie(x1, autopct=lambda pct: func(pct, x1),
+                                  textprops=dict(color="w"))
+    axes[0].legend(wedges, labels, loc=(1.0, 0.6))
+    axes[0].set_title(f"Revenue comparison in last year")
+
+    wedges, texts, autotexts = axes[1].pie(x2, autopct=lambda pct: func(pct, x2),
+                                  textprops=dict(color="w"))
+    axes[1].legend(wedges, labels, loc=(1.0, 0.6))
+    axes[1].set_title(f"Revenue comparison in last month")
+    plt.savefig(img_url)
+
+    kwargs = dict(
+        direct_ly=direct_ly,
+        indirect_ly=indirect_ly,
+        direct_lm=direct_lm,
+        indirect_lm=indirect_lm,
+        img_url=img_url
+    )
+    return render_template("staff_compare_revenue.html", **kwargs)
 
 
 @ app.route("/staff_view_top_destinations")
 def staff_view_top_destinations():
-    return "TODO"
+    if "usertype" not in session:
+        return redirect("/")
+    if session["usertype"] != "staff":
+        return redirect("/")
+    cursor = conn.cursor()
+    query = """
+            select airport_city
+            from (purchases natural join ticket natural join flight), airport
+            where arrival_airport = airport_name
+            and purchase_date > '{}'
+            and airline_name = '{}'
+            group by airport_city
+            order by count(*)
+            desc
+            limit 3
+            """
+    last_year = add_months(datetime.date.today(), -12).strftime('%Y-%m-%d')
+    last_3months = add_months(datetime.date.today(), -3).strftime('%Y-%m-%d')
 
+    cursor.execute(query.format(last_year, session["airline_name"]))
+    dsts_ly = [row[0] for row in cursor.fetchall()]
+
+    cursor.execute(query.format(last_3months, session["airline_name"]))
+    dsts_l3m = [row[0] for row in cursor.fetchall()]
+    cursor.close()
+
+    kwargs = dict(
+        dsts_ly=dsts_ly,
+        dsts_l3m=dsts_l3m
+        )
+    return render_template("staff_view_top_destinations.html", **kwargs)
 
 @ app.route("/show_sessions")
 def show_sessions():
